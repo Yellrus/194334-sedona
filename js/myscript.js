a4afba91f569a1f09e5f08bb8ebd865eaa2c803b
Yellrus <ruslan.namanov@yandex.ru>
2016-05-04 20:16:34 +0300

    усё

--- js/myscript.js
+++ js/myscript.js
@@ -2,11 +2,10 @@ var link = document.querySelector(".search-button");
 var searchForm = document.querySelector(".search-form");
 
 link.addEventListener("click", function(event) {
-event.preventDefault();
-if (searchForm.classList.contains("search-form-visible")) {
-searchForm.classList.remove("search-form-visible");
-}
-else {
-searchForm.classList.add("search-form-visible");
-};
+  event.preventDefault();
+  if (searchForm.classList.contains("search-form-visible")) {
+    searchForm.classList.remove("search-form-visible");
+  } else {
+    searchForm.classList.add("search-form-visible");
+  };
 });
